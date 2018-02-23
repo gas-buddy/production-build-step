714945e7f617b9c1b3e38120dcce98a9aba89586
Jean-Charles Sisk <jasisk@gmail.com>
2018-02-23 16:34:45 -0500

    validate production.json

--- run.sh
+++ run.sh
@@ -5,4 +5,11 @@ echo HASH=$WERCKER_GIT_COMMIT >> BUILD_INFO
 echo $WERCKER_BUILD_ID > WERCKER_BUILD_ID
 
 export NODE_ENV=production
+VALIDATE="require('shush')('./config/production.json')"
+
+if [ -f ./config/production.json ] && node -e "${VALIDATE}"; then
+  echo "ERROR: Your production.json did not validate." >&2
+  exit 1
+fi
+
 npm run build
